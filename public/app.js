ca04110a082c6afd45d07238c8df928a2ea875a2
sudiptanandan23 <sudiptanandan23@gmail.com>
2026-03-12 11:52:43 +0530

    Update app.js

--- public/app.js
+++ public/app.js
@@ -3,17 +3,27 @@ let user = JSON.parse(localStorage.getItem("user"))
 function register(){
 
 fetch("/register",{
+
 method:"POST",
 headers:{'Content-Type':'application/json'},
+
 body:JSON.stringify({
+
 name:document.getElementById("name").value,
 email:document.getElementById("email").value,
 password:document.getElementById("password").value
+
 })
+
 })
 .then(()=>{
-alert("Registered successfully")
+
+sendDataCloudEvent("UserRegistered")
+
+alert("Registered")
+
 window.location="login.html"
+
 })
 
 }
@@ -21,21 +31,35 @@ window.location="login.html"
 function login(){
 
 fetch("/login",{
+
 method:"POST",
 headers:{'Content-Type':'application/json'},
+
 body:JSON.stringify({
+
 email:document.getElementById("email").value,
 password:document.getElementById("password").value
+
 })
+
 })
+
 .then(res=>res.json())
+
 .then(data=>{
 
 if(data.success){
+
 localStorage.setItem("user",JSON.stringify(data.user))
+
+sendDataCloudEvent("UserLogin")
+
 window.location="products.html"
+
 }else{
+
 alert("Invalid login")
+
 }
 
 })
@@ -45,7 +69,9 @@ alert("Invalid login")
 function loadProducts(){
 
 fetch("/products")
+
 .then(res=>res.json())
+
 .then(products=>{
 
 const container=document.getElementById("products")
@@ -53,13 +79,19 @@ const container=document.getElementById("products")
 products.forEach(p=>{
 
 let div=document.createElement("div")
+
 div.className="card"
 
 div.innerHTML=`
+
 <img src="${p.image}">
+
 <h3>${p.name}</h3>
+
 <p>$${p.price}</p>
+
 <button onclick='addToCart(${JSON.stringify(p)})'>Add to Cart</button>
+
 `
 
 container.appendChild(div)
@@ -73,13 +105,21 @@ container.appendChild(div)
 function addToCart(product){
 
 fetch("/cart/add",{
+
 method:"POST",
+
 headers:{'Content-Type':'application/json'},
+
 body:JSON.stringify({
+
 email:user.email,
 product:product
+
 })
+
 })
+
+sendDataCloudEvent("AddToCart",product)
 
 alert("Added to cart")
 
@@ -88,7 +128,9 @@ alert("Added to cart")
 function loadCart(){
 
 fetch(`/cart/${user.email}`)
+
 .then(res=>res.json())
+
 .then(items=>{
 
 const div=document.getElementById("cart")
@@ -96,9 +138,62 @@ const div=document.getElementById("cart")
 items.forEach(i=>{
 
 let p=document.createElement("p")
-p.innerText=i.name+" - $"+i.price
+
+p.innerText=i.name + " - $" + i.price
 
 div.appendChild(p)
+
+})
+
+})
+
+}
+
+function checkout(){
+
+fetch("/checkout",{
+
+method:"POST",
+
+headers:{'Content-Type':'application/json'},
+
+body:JSON.stringify({
+
+email:user.email
+
+})
+
+})
+
+sendDataCloudEvent("Purchase")
+
+alert("Order placed!")
+
+window.location="index.html"
+
+}
+
+function sendDataCloudEvent(eventType,product={}){
+
+fetch("https://YOUR-DATACLOUD-ENDPOINT/events",{
+
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify({
+
+eventType:eventType,
+
+email:user?.email,
+
+productName:product.name,
+
+price:product.price,
+
+timestamp:new Date()
 
 })
 
